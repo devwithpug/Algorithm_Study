9619ba7df5f36ed83fcb46948250fb26b1a50e57
devwithpug <zmfjscl789@gmail.com>
2021-05-03 00:08:40 +0900

    210503

--- python/goorm/정렬/goorm_43205.py
+++ python/goorm/정렬/goorm_43205.py
@@ -1,19 +1,29 @@
 # goorm / 정렬 / 오늘의 업무
 # https://level.goorm.io/exam/43205/%EC%98%A4%EB%8A%98%EC%9D%98-%EC%97%85%EB%AC%B4/quiz/1
+# 위상정렬 알고리즘
 
 n, r = map(int, input().split())
-arr = {'A':0,'B':1,'C':2,'D':3,'E':4,'F':5}
+arr = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F'}
+path = {}
+queue = []
+
+for i in range(n):
+    path.setdefault(arr[i], [])
+    queue.append(arr[i])
 
 for _ in range(r):
     a, b = map(str, input())
-    if arr[a] > arr[b]:
-        arr[a], arr[b] = arr[b], arr[a]
-    
-for i in range(n):
-    for key, item in arr.items():
-        if i == item:
-            print(key, end="")
-            
-            
-# DFS를 이용한 위상정렬 공부하기
-#UNSOLVED
+    path[b].append(a)
+
+print(path)
+
+while len(queue) > 0:
+    for item in queue:
+        if len(path[item]) > 0:
+            continue
+        for k in path.keys():
+            if item in path[k]:
+                path[k].remove(item)
+        print(item, end="")
+        queue.remove(item)
+        break
