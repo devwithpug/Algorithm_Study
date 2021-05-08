d925ccc285b1020e2cc1118076957dce994cfcc5
devwithpug <zmfjscl789@gmail.com>
2021-05-09 00:26:43 +0900

    210509

---
+++ python/goorm/기타/goorm_43244.py
@@ -0,0 +1,6 @@
+# goorm / 기타 / 부모 단어
+# https://level.goorm.io/exam/43244/%EB%B6%80%EB%AA%A8-%EB%8B%A8%EC%96%B4/quiz/1
+
+p, c = input().split()
+
+print("{}".format("YES" if set(p) == set(c) else "NO"))
